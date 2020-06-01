0dc5feef4f7c439d619acd10847eba2135550ac2
Lijamaija <liza.timofeeva@gmail.com>
2020-06-01 22:53:31 +0200

    more work on cards

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+const Chart = () => {
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect(() => {
+      const fetchAPI = async () => {
+         setDailyData(await fetchDailyData());
+      }
+      fetchAPI();
+    });
+
+    return(
+        <h1></h1>
+    )
+};
+
+export default Chart;
